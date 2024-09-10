5b770b6387d9783a980efa1238164091af1a1235
Jason Palma <jasonpalma16@gmail.com>
2024-09-10 13:16:53 -0400

    Start project

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace _9_10_homework1a {
+    class Program {
+        static void Main(string[] args) {
+            
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,7 @@
+# CSC 350H Homework 1-a
+
+Homework for my CSC 350H course 
+
+## How to download
+
+(W.I.P)
