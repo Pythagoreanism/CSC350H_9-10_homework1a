947c2bf50089f3d2e9cff55bf843f44a05008426
Jason Palma <jasonpalma16@gmail.com>
2024-09-11 11:30:06 -0400

    Import Cards2 dll

--- Program.cs
+++ Program.cs
@@ -1,9 +1,10 @@
 ﻿using System;
+using Cards2;
 
 namespace _9_10_homework1a {
     class Program {
         static void Main(string[] args) {
-            
+            Deck deck = new Deck();
         }
     }
 }
